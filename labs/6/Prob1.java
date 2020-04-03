5084b79932bcada2b63867aaf0826aa7b44f1af0
jacobingebrigtsen1 <54602825+jacobingebrigtsen1@users.noreply.github.com>
2020-04-03 16:57:07 -0400

    Add files via upload

---
+++ labs/6/Prob1.java
@@ -0,0 +1,10 @@
+
+public class Prob1 {
+	public static void main(String[] args) {
+		  Square w1 = new Square(40);
+		  Square w2 = new Square(35.9);
+		  System.out.println(w1);
+		  System.out.println();
+		  System.out.println(w2);
+	}
+}
